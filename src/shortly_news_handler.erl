8f8da73cb4a9be74681e7587e05849d08d591c1b
Amin Arria <arria.amin@gmail.com>
2018-06-05 18:26:48 -0300

    Removed comment

--- src/shortly_news_handler.erl
+++ src/shortly_news_handler.erl
@@ -7,17 +7,6 @@
 init(Req, State) ->
   {cowboy_websocket, Req, State}.
 
-
-  % curl --include \
-  %      --no-buffer \
-  %      --header "Connection: Upgrade" \
-  %      --header "Upgrade: websocket" \
-  %      --header "Host: localhost:8080" \
-  %      --header "Origin: http://localhost:8080" \
-  %      --header "Sec-WebSocket-Key: SGVsbG8sIHdvcmxkIQ==" \
-  %      --header "Sec-WebSocket-Version: 13" \
-  % http://localhost:8080/news
-
 websocket_init(State) ->
     pg2:join(ws_connections, self()),
     {ok, State}.
